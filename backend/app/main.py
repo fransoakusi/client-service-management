251a13596a4c31eca210a8ad9caf0608ba6ec432
fransoakusi <fkusi686@gmail.com>
2025-01-10 16:43:00 +0000

    Changes for backend success to display on a browser

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,5 +2,12 @@ from app import create_app
 
 app = create_app()
 
+
+@app.route("/")
+def home():
+    return "<h1>Welcome to the Client Service Management System</h1><p>This is the backend server.</p>"
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
+
+ 
